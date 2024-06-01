fd8898e05bb8aca5cd03e4a7cb8e5d59107cfb21
Alan Meeson <alan@carefullycalculated.co.uk>
2024-06-01 14:29:22 +0100

    Format and clean Sphinxs config and index.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,30 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'LanceDB Haystack Document Store'
-copyright = '2024, Alan Meeson'
-author = 'Alan Meeson'
-release = '0.1.0'
+project = "LanceDB Haystack Document Store"
+copyright = "2024, Alan Meeson"  # noqa: A001
+author = "Alan Meeson"
+release = "0.1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints", "myst_parser"]
-
-templates_path = ['_templates']
+extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints", "myst_parser", "autoapi.extension"]
+templates_path = ["_templates"]
 exclude_patterns = []
 
-
+autoapi_dirs = ["../../src/lancedb_haystack"]
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_theme_options = {"collapse_navigation": False}
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,6 +10,7 @@
    :maxdepth: 2
    :caption: Contents:
 
+   self
 
 
 Indices and tables
